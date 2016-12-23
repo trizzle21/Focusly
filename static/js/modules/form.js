276dd22d0583d472b983e180b68632cf998b8e94
trizzle21 <tdross21@gmail.com>
2016-12-22 23:09:20 -0800

    outlined form;

---
+++ static/js/modules/form.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+class EntryForm extends React.Component {
+	constructor() {
+		this.state = {
+			numOfSessions: '',
+			WorkMusicType: '',
+			RestMusicType: '',
+
+		};
+
+	}
+}
