e35cff5fe76d47a36da462d755e8b3051d820863
trizzle21 <tdross21@gmail.com>
2016-12-22 23:14:59 -0800

    imported the right fields for the menu

--- static/js/modules/form.js
+++ static/js/modules/form.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import SelectField from material-ui;
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+import TextField from 'material-ui/TextField';
 
 
 class EntryForm extends React.Component {
