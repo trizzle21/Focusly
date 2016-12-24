141995610a70254a790273a666ee8a0b1fb687f7
trizzle21 <tdross21@gmail.com>
2016-12-23 22:59:47 -0800

    almost finished setup form

--- static/js/modules/form.js
+++ static/js/modules/form.js
@@ -9,23 +9,43 @@ class EntryForm extends React.Component {
 	constructor() {
 		this.state = {
 			numOfSessions: 0,
-			WorkMusicType: '',
-			RestMusicType: '',
+			WorkMusicType: 1,
+			RestMusicType: 1,
+			SessionSlider: 0.0,
 
 		};
-
+	}
 	
    	handleSubmit(event) {
 	 	
 	}
 
+  	handleSliderChange (event, value) {
+  		this.setState({SessionSlider:value})
   	}
 
   	render() {
   		//render form here
   		return (
 
-    
+  		<Slider step={1.0} value={this.state.SessionSlider} onChange={this.handleSliderChange} min={0} max={10}/>
+  		<h4>{this.state.SessionSlider}</h4>
+        <SelectField
+          floatingLabelText="Working Music"
+          value={this.state.WorkMusicType}
+          onChange={this.WorkMusicChange}
+        >
+        	<MenuItem value={1} primaryText="Movie Soundtrack" />
+        	<MenuItem value={2} primaryText="Relaxing" />
+    	    <MenuItem value={3} primaryText="Classical" />
+	        <MenuItem value={4} primaryText="Surprise Me" />
+
+        </SelectField>
+
+
+
+
+
 
     	<IconButton iconClassName="muidocs-icon-custom-spotify" disabled={true} />
 
