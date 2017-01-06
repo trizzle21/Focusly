4b02d32c0d836cda9a59fe09793cb789a909a59f
trizzle21 <tdross21@gmail.com>
2017-01-05 17:31:15 -0800

    added timer

---
+++ static/js/modules/timer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import CircularProgress from 'material-ui/CircularProgress';
+
+
+
+
+class Timer extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {	
+			completed: 100,
+			timer: 25, //this.props.time
+		};
+	}
+
+
+
+	render() {
+
+
+		return (
+
+
+		)
+	}
