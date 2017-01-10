66efd0d7b171af1c104967c6ce09c8aad7e8135b
trizzle21 <tdross21@gmail.com>
2017-01-09 23:40:55 -0800

    lots of project changes

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ var app = express()
 
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello Dee!')
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Other app listening on port 3000!')
 })
