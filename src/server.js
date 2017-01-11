b7e81aeb66233f5fb89dece8f4fe96848cd96c0c
trizzle21 <tdross21@gmail.com>
2017-01-11 14:20:30 -0800

    added index (aka learning to use pug)

--- src/server.js
+++ src/server.js
@@ -5,8 +5,9 @@ app.set('view engine', 'pug')
 
 
 app.get('/', function (req, res) {
-  res.render('home.html')
+  res.render('index', { title: 'Hey', message: 'Hello there!' })
 })
+
 
 app.listen(3000, function () {
   console.log('Other app listening on port 3000!')
