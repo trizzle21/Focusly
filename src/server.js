35064fbb884bd36353eab6971110ac031c758ab7
trizzle21 <tdross21@gmail.com>
2017-01-11 21:42:33 -0800

    got the templating system working

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 var express = require('express')
 var app = express()
 
+app.set('views', './views')
 app.set('view engine', 'pug')
 
 
 app.get('/', function (req, res) {
-  res.render('index', { title: 'Hey', message: 'Hello there!' })
+  res.render('index', { message: 'Sup there!' })
 })
 
 
