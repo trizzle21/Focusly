c66bd3fc7387409e56567403ad86b143dc88046c
trizzle21 <tdross21@gmail.com>
2017-01-23 00:48:41 -0800

    started adding actions and reducers

---
+++ src/client/actions/index.js
@@ -0,0 +1,11 @@
+import Spotify from 'spotify-web-api-js';
+const spotifyApi = new Spotify();
+
+
+
+export const SPOTIFY_TOKENS = 'SPOTIFY_TOKENS';
+export const SPOTIFY_ME_BEGIN = 'SPOTIFY_ME_BEGIN';
+export const SPOTIFY_ME_SUCCESS = 'SPOTIFY_ME_SUCCESS';
+export const SPOTIFY_ME_FAILURE = 'SPOTIFY_ME_FAILURE';
+
+
