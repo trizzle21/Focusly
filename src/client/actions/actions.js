343a762454daf7e0b5b176f0a74b944eae8e5f0d
trizzle21 <tdross21@gmail.com>
2017-01-29 15:48:04 -0800

    added more actions

--- src/client/actions/actions.js
+++ src/client/actions/actions.js
@@ -1,18 +1,12 @@
 import Spotify from 'spotify-web-api-js';
+import * as types from '../constants/ActionTypes';
+
 
 const spotifyApi = new Spotify();
 
 
 
 //Spotify Login
-export const SPOTIFY_TOKENS = 'SPOTIFY_TOKENS';
-export const SPOTIFY_REC_BEGIN = 'SPOTIFY_ME_BEGIN';
-export const SPOTIFY_REC_SUCCESS = 'SPOTIFY_ME_SUCCESS';
-export const SPOTIFY_REC_FAILURE = 'SPOTIFY_ME_FAILURE';
-
-export const SPOTIFY_GENRE_SEED_BEGIN = 'SPOTIFY_ME_BEGIN';
-export const SPOTIFY_GENRE_SEED_SUCCESS = 'SPOTIFY_ME_SUCCESS';
-export const SPOTIFY_GENRE_SEED_FAILURE = 'SPOTIFY_ME_FAILURE';
 
 
 export function setTokens({accessToken, refreshToken}) {
@@ -68,16 +62,6 @@ export const makeAction('RECOMMENDATION_SET');
 
 
 //Session Form
-export const SLIDER_INCR = 'SLIDER_INCR';
-export const SLIDER_DECR = 'SLIDER_DECR';
-
-export const WORK_MUSIC_SELECT = 'WORK_MUSIC_SELECT';
-export const REST_MUSIC_SELECT = 'REST_MUSIC_SELECT';
-
-
-
-
-
 export const workMusicSelect = makeAction('WORK_MUSIC_SELECT', 'musicType' );
 export const restMusicSelect = makeAction('REST_MUSIC_SELECT', 'musicType' );
 
@@ -85,10 +69,6 @@ export const restMusicSelect = makeAction('REST_MUSIC_SELECT', 'musicType' );
 
 
 //Timer Data
-export const CYCLE_SET = 'CYCLE_SET';
-export const SESSION_TYPE_SET = 'SESSION_TYPE_SET';
-export const TICK = 'TICK';
-
 
 
 export const cycleSet = makeAction(CYCLE_SET, data );
